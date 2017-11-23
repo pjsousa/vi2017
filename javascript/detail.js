f7091ee4eae51c5ad0d53915aada5ccfe5e868db
Pedro Sousa <pjgs.sousa@gmail.com>
2017-11-23 20:18:55 +0000

    the detail card now has an awefull, incomplete bullet chart.

--- javascript/detail.js
+++ javascript/detail.js
@@ -1,4 +1,5 @@
 ;(function(){
+	
 	function drawdtl(){
 		var dataset = appstate.highlightedRows[0];
 
@@ -21,7 +22,76 @@
 	};
 
 	function drawBullet(){
+		var w_sales = 500;
+		var h_sales = 50;
+		var padding_sales = 5;
+		var xoffset_sales = 0;
+		var yoffset_sales = 0;
+		var xcutoff_sales = 0;
+		var ycutoff_sales = 0;
+
+		var total_var = "Global_Sales";
+		var detail_vars = ["NA_Sales", "EU_Sales", "JP_Sales", "Other_Sales"];
+
+		var isLogScale = false;
+		var isCenteredData = false;
+
+		var xscale = null;
+
+
+
+
+		var row_number = appstate.highlightedRows[0];
+
+		var svg = d3.select("#dtlPanel").selectAll("#salesViz");
+
+		svg.attr("width",w_sales)
+			.attr("height",h_sales);
 		
+		var xdomain = [];
+		xdomain[0] = 0
+		xdomain[1] = data_utils.read_value(row_number, total_var);
+
+		var xrange = [];
+		xrange[0] = padding_sales + xoffset_sales;
+		xrange[1] = w_sales-padding_sales;
+
+		var yrange = [];
+		yrange[0] = padding_sales;
+		yrange[1] = h_sales - padding_sales - yoffset_sales;
+
+		if(isLogScale){
+			xscale = d3.scaleLog();
+		}
+		else{
+			xscale = d3.scaleLinear();
+		}
+
+		xscale.domain(xdomain)
+			.range(xrange);
+
+		var pixelvalues;
+		var pixeloffsets;
+
+		pixelvalues = detail_vars.map(function(d){ return xscale( data_utils.read_value(row_number, d)); });
+		pixeloffsets = pixelvalues.slice(1, pixelvalues.length)
+				.map(function(d,i){ return d3.sum(pixelvalues.slice(0, i+1)); });
+		pixeloffsets.unshift(0);
+
+		svg.selectAll("rect")
+			.data(detail_vars)
+			.enter().append("rect");
+			svg.selectAll("rect")
+				.transition()
+				.duration(500)
+				.attr("x", function(d, i){ return pixeloffsets[i]; })
+				.attr("y", yrange[0])
+				.attr("width", function(d, i){ return pixelvalues[i] })
+				.attr("height", yrange[1]-yrange[0])
+				.attr("fill", function(d,i){ return ["red", "blue", "yellow", "tomato"][i] });
+
+
+
 	}
 
 	window.drawdtl = drawdtl;
