799c32e1b3770e731a189b962df1173ee0c58e01
Pedro Sousa <pjgs.sousa@gmail.com>
2017-12-17 15:25:37 +0000

    Added bottom highlight to cleveland

--- javascript/cleveland.js
+++ javascript/cleveland.js
@@ -3,9 +3,15 @@
 	var h = -1;
 	var padding = 30;
 	var xoffset = 200;
-	var yoffset = 40;
+	var yoffset = 60;
 	var xcutoff = 0;
-	var ycutoff = 0;
+	var ycutoff = 40;
+
+	var xoffset_h = 200;
+	var yoffset_h = 20;
+	var xcutoff_h = 0;
+	var ycutoff_h = 0;
+
 	var max_names_len = 40;
 	var top_rows = 15;
 	var r = 5;
@@ -32,12 +38,15 @@
 	var yrange = null;
 	var xscale = null;
 	var yscale = null;
+	var xscale_h = null;
+	var yscale_h = null;
 
 	var axis_0;
 	var yaxis;
 	var xaxis;
 
 	var dataset;
+	var dataset_h;
 	var rows_order;
 
 	var svg;
@@ -144,6 +153,10 @@
 				.duration(100)
 				.attr("r", r+1)
 		}
+
+		if(from_target != "clv"){
+			updatePlotHighlight(row_nums.slice(0,1));
+		}
 	};
 
 	function setSizesclv(boundingRect){
@@ -169,7 +182,7 @@
 		ydomain[0] = 0
 		ydomain[1] = top_rows - 1;
 		xdomain = [];
-		xdomain[0] = 0.95 * d3.min(rows_order, function(d) { return parseFloat(raw_value(d, x_var)) });
+		xdomain[0] = 0;
 		xdomain[1] = 1.05 * d3.max(rows_order, function(d) { return parseFloat(raw_value(d, x_var)) });
 
 
@@ -208,6 +221,8 @@
 		// 4) Creating the scales
 		yscale = d3.scaleLinear();
 		xscale = d3.scaleLinear();
+		yscale_h = d3.scaleLinear();
+		xscale_h = d3.scaleLinear();
 		
 		yscale.domain(ydomain)
 				.range(yrange);
@@ -249,6 +264,19 @@
 				.attr("transform","translate("+ axis_0["x"] +",0)")
 				.attr("class", "y axis");
 
+		// for the bottom highlight
+		svg.selectAll("g.background-h")
+			.data([0]).enter().append("g")
+				.attr("class", "background-h");
+
+		svg.selectAll("g.x.axis-h")
+			.data([0]).enter().append("g")
+				.attr("class", "x axis-h")
+
+		svg.selectAll("g.y.axis-h")
+			.data([0]).enter().append("g")
+				.attr("class", "y axis-h");
+
 
 
 		// X) Append group for data points grid
@@ -283,7 +311,7 @@
 			.enter().append("text")
 			.attr("class", "x-axis-label")
 			.attr("x", xrange[1])
-			.attr("y", yrange[1] + yoffset)
+			.attr("y", yrange[0] - ycutoff)
 			.attr("fill", "black")
 			.style("text-anchor", "end")
 			.text("Global Sales ( Million Units )");
@@ -355,11 +383,13 @@
 
 		// Draw the X ticks and x label
 		svg.select("g.background").selectAll("text.x-axis-tick")
+			.remove();
+		svg.select("g.background").selectAll("text.x-axis-tick")
 			.data(xscale.ticks())
 			.enter().append("text")
 				.attr("class", "x-axis-tick")
 				.attr("x", function(d){ return xscale(d) })
-				.attr("y", yrange[1] + 25)
+				.attr("y", yrange[0] - ycutoff + 20)
 				.attr("fill", "black")
 				.style("text-anchor", "middle")
 				.style("font-size", 10)
@@ -436,7 +466,129 @@
 				
 				appdispatch.gameout.call("gameout", this, d, "clv");
 			});
-	}
+	};
+
+	function updatePlotHighlight(row_numbers){
+		var dataset = row_numbers;
+		dataset_h = dataset;
+
+		//updatePanelHeader();
+
+		// update plot
+		var t0 = svg.transition().delay(100).duration(300);
+		var t1 = svg.transition().duration(100);
+
+		if(row_numbers.length !== 1){
+			svg.select("g.datapoints-h").selectAll("circle.data-points")
+				.transition(t1)
+				.remove();
+
+			svg.select("g.background-h").selectAll("text")
+				.text("");
+			return;
+		}
+
+		// 1) Settle the values for the x and y domains (this are the values in the data)
+		ydomain = [];
+		ydomain[0] = 0
+		ydomain[1] = 1;
+
+		// 2) Settle the values for the x and y ranges (this are the values/dimensions in pixels)
+		var yrange = [];
+		yrange[0] = h - padding - ycutoff + yoffset_h
+		yrange[1] = h - padding - ycutoff_h
+		var xrange = [];
+		xrange[0] = padding + xoffset;
+		xrange[1] = w - padding - xcutoff;
+
+
+
+		// 4) Creating the scales
+		var yscale_h = d3.scaleBand();
+
+		yscale_h.domain(d3.range.apply(null, ydomain))
+			.range(yrange)
+
+		// 6) Create the background grid
+		// Draw the Y grid
+		svg.select("g.background-h").selectAll("line.y-grid")
+			.data(dataset_h)
+			.enter().append("line")
+				.attr("class", "y-grid")
+				.attr("x1", xrange[0])
+				.attr("x2", xrange[1])
+				.attr("stroke-width", 1)
+				.attr("stroke", "rgba(120,120,120,0.2)");
+		svg.select("g.background-h").selectAll("line.y-grid")
+			.data(dataset_h)
+			.exit().remove();
+		svg.select("g.background-h").selectAll("line.y-grid")
+			.transition(t1)
+			.attr("y1", function(d, i){ return yscale_h(i); })
+			.attr("y2", function(d, i){ return yscale_h(i); });
+
+		// Draw the Y ticks and y label
+		svg.select("g.background-h").selectAll("text.y-axis-tick")
+			.data(dataset_h)
+			.exit().remove();
+		svg.select("g.background-h").selectAll("text.y-axis-tick")
+			.data(dataset_h)
+			.enter().append("text")
+				.attr("class", "y-axis-tick")
+				.attr("x", xrange[0])
+				.attr("y", function(d, i){ return yscale_h(i); })
+		svg.select("g.background-h").selectAll("text.y-axis-tick")
+				.transition()
+				.duration(500)
+				.attr("x", xrange[0])
+				.attr("y", function(d,i){ return yscale_h(i); })
+				.attr("dx", "-5px")
+				.attr("dy", "1px")
+				.attr("y", function(d,i){ return yscale_h(i); })
+				.attr("fill", "black")
+				.style("cursor", "default")
+				.style("text-anchor", "end")
+				.style("font-size", 10)
+				.text(function(d, i){ 
+					var result = null;
+					var game_name = raw_value(d, y_var);
+
+					result = _.truncate(game_name, {'length': max_names_len, 'omission': '...'});
+
+					return result;
+				})
+
+
+		// 7) Plot the data itself
+		// draws the plot itself
+		svg.select("g.datapoints-h").selectAll("circle.data-points")
+			.data(dataset_h)
+			.enter().append("circle")
+			.attr("id", function(d){ return "d-clv-"+ dataset_h[d]; })
+			.attr("class", "data-points")
+			.attr("cx", xrange[0])
+			.attr("cy",function(d, i) {
+				return yscale_h(i);
+			})
+		svg.select("g.datapoints-h").selectAll("circle.data-points")
+			.data(dataset_h)
+			.exit().remove();
+		svg.select("g.datapoints-h").selectAll("circle.data-points")
+			.data(dataset_h)
+			.transition()
+			.delay(500)
+			.duration(500)
+			.attr("r",r)
+			.attr("fill","rgb(255,0,255)")
+			.style("cursor", "none")
+			.attr("cx",function(d, i) {
+				var v = value(d, x_var);
+				return  xscale(v);
+			})
+			.attr("cy",function(d, i) {
+				return yscale_h(i);
+			})
+	};
 
 	function drawclv(app_row_numbers, svgelement){
 		if(typeof svgelement === "undefined"){
